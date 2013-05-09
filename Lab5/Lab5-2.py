18a5761c0043ddca6a1dfde67d77bdb8c11fd740
Sebastiano Milardo <s.milardo@hotmail.it>
2013-05-09 21:16:54 +0200

    Minor changes
    Added some comments

--- Lab5/Lab5-2.py
+++ Lab5/Lab5-2.py
@@ -9,11 +9,13 @@ from __future__ import division, print_function
 import random
 import nltk
 import re
+import math
 from nltk.collocations import BigramCollocationFinder
 from nltk.collocations import TrigramCollocationFinder
 
 from nltk.corpus import movie_reviews
 from nltk.corpus import stopwords
+from collections import Counter, defaultdict
 from collections import Counter
 from nltk import bigrams
 from nltk import trigrams
@@ -32,45 +34,47 @@ from nltk import trigrams
 
 
 
-documents = []
-for category in movie_reviews.categories():
-    file_ids = movie_reviews.fileids(category)
-    for fileid in file_ids:
-        documents.append( [movie_reviews.words(fileid), category] )
+#==============================================================================
+#  Helper functions
+#==============================================================================
 
-all_words = nltk.FreqDist(w for w in movie_reviews.words())
-
-
-wnl = nltk.WordNetLemmatizer()
-p_stemmer = nltk.PorterStemmer()
-l_stemmer = nltk.LancasterStemmer()
+def build_inverted_index(documents, keywords):
+    '''
+    Build inverted index for each keyword in a form
+    {'keyword': {<document index>: TF, ..., 'df': DF, 'idf': IDF}, ...}
+    where TF is a frequency of keyword in a document, DF is number
+    of documents which contains the keyword and IDF is a measure of
+    informativeness of the keyword
+    '''
+    index = defaultdict(dict)
+    counters = [Counter(d) for d,c in documents]
+    N = len(documents)
+    for keyword in keywords:
+        df = 0
+        for i, counter in enumerate(counters):
+            if counter[keyword]:
+                index[keyword][i] = counter[keyword]
+                df += 1
+        index[keyword]['df'] = df
+        index[keyword]['idf'] = math.log(N / df, 10)
+    return index
 
 
 #==============================================================================
-#  Create bigrams - trigrams - collocations
+#  Features Function (document , words|bigrams|trigrams)
+#  returning a dictionary
 #==============================================================================
 
-
-    
-def create_collocations(features_words):
-    collocations=[]
-    return collocations   
-
-
-#==============================================================================
-#  Create Sets Functions
-#  Document Features improved
-#==============================================================================
-
-def count_collates_features(document, coll):
+def count_collocations_features(document, coll):
     features = {}
     return features
 
-def has_collates_features(document, coll):
+def has_collocations_features(document, coll):
     features = {}
     return features
 
 def count_trigrams_features(document, trigr):
+    '''Produces count features "count('word1','word2','word3'): <number>" '''
     features = {}
     for t in trigr:
         summ = 0
@@ -81,6 +85,7 @@ def count_trigrams_features(document, trigr):
     return features
 
 def count_bigrams_features(document, bigr):
+    '''Produces count features "count('word1','word2'): <number>" '''
     features = {}
     for b in bigr:
         summ = 0
@@ -91,6 +96,7 @@ def count_bigrams_features(document, bigr):
     return features
 
 def has_trigrams_features(document, trigr):
+    '''Produces binary features "has('word1','word2','word3'): <true|false>" '''
     features = {}
     for t in trigr:
         found = False
@@ -102,6 +108,7 @@ def has_trigrams_features(document, trigr):
     return features
     
 def has_bigrams_features(document, bigr):
+    '''Produces binary features "has('word1','word2'): <true|false>" '''
     features = {}
     for b in bigr:
         found = False
@@ -114,32 +121,34 @@ def has_bigrams_features(document, bigr):
 
 
 def has_features(document, features_words):
+    '''Produces binary features "has('word'): <true|false>" '''
     features = {}
     for word in document:
         features['has(%s)' % word] = (word in features_words)
     return features
 
 def count_features(document, features_words):
+    '''Produces count features "count('word'): <number>" '''
     features = {}
     counter = Counter(document)
     for word in features_words:
         features['count(%s)' % word] = counter[word]
     return features
 
-
+## Function which builds up training/testing set
 def create_sets(documents, features_words):
     featuresets = []
     bigr = bigrams(features_words)
     trigr = trigrams(features_words)
-    coll = create_collocations(features_words)    
+    #coll = create_collocations(features_words)    
     
     l = len(documents)
     for i in range(l):
         print("Computing the feature set for document {0} of {1}".format(i,l))
         features = {}
         #features.update(has_features(document[0], features_words))
-        #features.update(has_bigrams_features(document[0], bigr))
-        features.update(has_trigrams_features(documents[i][0], trigr))
+        features.update(has_bigrams_features(document[0], bigr))
+        #features.update(has_trigrams_features(documents[i][0], trigr))
         #features.update(has_collates_features(document[0], collates))
         #features.update(count_features(document[0], features_words))
         #features.update(count_bigrams_features(document[0], bigr))
@@ -181,25 +190,40 @@ def evaluate(classifier, test_set):
 
 
 #==============================================================================
-#  Analysis definition
+#  Analysis function
 #==============================================================================
 def analysis(documents, document_preprocess, features, features_preprocess):
     for f in document_preprocess:
-        for i, document in enumerate(documents):
-            documents[i][0] = map(f, document[0])
+        for i in range(len(documents)):
+            documents[i][0] = map(f, documents[i][0])
         features = map(f, features)
 
     for feat_func in features_preprocess:
         features = feat_func(features)
-    features = set(features[:1000])
-#    features = features[:1000]
+    features = set(features)
 
     train_set, test_set = create_sets(documents, features)
     classifier = nltk.NaiveBayesClassifier.train(train_set)
 
     x = evaluate(classifier, test_set)
-    classifier.show_most_informative_features(n=20)
+    #~ classifier.show_most_informative_features(n=20)
     return x
+
+#==============================================================================
+#  Features generators
+#==============================================================================
+
+def freq_features(features):
+    return nltk.FreqDist(features).keys()[:1000]
+
+
+def tf_idf_features(features):
+    # needs documents here :/
+    global documents  # NOOOOOOOOO!!!!!!!!!!!! Blah!!!
+
+#==============================================================================
+#  Helper functions for features cleaning
+#==============================================================================
 
 def punctuation_remove(features):
     processed = []
@@ -217,26 +241,43 @@ def stopwords_remove(features):
         processed.append(word)
     return processed
 
+#==============================================================================
+#  Program main block
+#==============================================================================
+
+documents = []
+for category in movie_reviews.categories():
+    file_ids = movie_reviews.fileids(category)
+    for fileid in file_ids:
+        documents.append( [movie_reviews.words(fileid), category] )
+
+all_words = movie_reviews.words()[:]
+
+wnl = nltk.WordNetLemmatizer()
+p_stemmer = nltk.PorterStemmer()
+l_stemmer = nltk.LancasterStemmer()
+
 results = []
 # (document/features preprocessing functions, features cleaning functions)
 analysis_functions = [
-    ((str.lower, ),      (),),
-    ((wnl.lemmatize, ),  (),),
-    ((p_stemmer.stem, ), (),),
-    ((),                 (punctuation_remove, ),),
-    ((),                 (punctuation_remove, stopwords_remove),),
-    ((wnl.lemmatize, ),  (stopwords_remove, ),),
-    ((p_stemmer.stem, ), (stopwords_remove, ),),
-    ((),                 (stopwords_remove, ),),
+    ((str.lower, ),      (freq_features, )),
+    ((wnl.lemmatize, ),  (freq_features, )),
+    ((p_stemmer.stem, ), (freq_features, )),
+    ((),                 (punctuation_remove, freq_features)),
+    ((),                 (punctuation_remove, freq_features)),
+    ((wnl.lemmatize, ),  (stopwords_remove,   freq_features)),
+    ((p_stemmer.stem, ), (stopwords_remove,   freq_features)),
+    ((),                 (stopwords_remove,   freq_features)),
+    ((),                 (freq_features, )),
 ]
 
 SAMPLES = 1
 for i in range(SAMPLES):
      random.shuffle(documents)
      results.append([])
-     for doc_fs, feat_fs in analysis_functions:
+     for doc_clean, feat_clean in analysis_functions:
          results[i].append(
-             analysis(documents, doc_fs, all_words.keys()[:1000], feat_fs))
+             analysis(documents, doc_clean, all_words, feat_clean))
 
 for col in range(len(analysis_functions)):
     sums = [0, 0, 0, 0]
